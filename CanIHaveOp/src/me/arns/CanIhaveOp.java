3e0c0d4e7324b745ace271a73519a4fa5e17fa73
BuildTools <unconfigured@null.spigotmc.org>
2014-12-05 13:44:08 +1300

    Re-did code

--- CanIHaveOp/src/me/arns/CanIhaveOp.java
+++ CanIHaveOp/src/me/arns/CanIhaveOp.java
@@ -1,39 +1,47 @@
 package me.arns;
-
+ 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-
+ 
+/**
+ *
+ * @author Arns
+ */
 public class CanIhaveOp extends JavaPlugin {
-	
-	@Override
-	public void onEnable() {
-		getLogger().info("OnEnable Enabled");
-	}
-    
-	@Override
-	public void onDisable() {
-		getLogger().info("OnEnable Enabled");		
-		
-	}
-	
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		
-		if (cmd.getName().equalsIgnoreCase("canihaveop") && sender instanceof Player){
-			Player player = (Player) sender;
-			
-			player.sendMessage("Pending operator application for user: " + player.getName() +"!");
-			
-			player.sendMessage("Application denied for user: " + player.getName() + ChatColour.Red +"!");
-			return true;
-			
-		}	
-			
-		return false;
-		
-	}
-	
+ 
+    @Override
+    public void onEnable() {
+        getLogger().info("OnEnable Enabled");
+    }
+ 
+    @Override
+    public void onDisable() {
+        getLogger().info("OnEnable Enabled");
+ 
+    }
+ 
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+ 
+        if (cmd.getName().equalsIgnoreCase("canihaveop") && sender instanceof Player) {
+            Player player = (Player) sender;
+            player.sendMessage("Pending operator application for user: " + player.getName() + "!");
+            if (player.hasPermission("bukkit.op")) {
+                if (player.isOp()) {
+                    player.sendMessage(ChatColor.GREEN + "Application accepted for ADMIN: " + player.getName() + "!");
+                    player.setOp(true);
+                } else {
+                    player.sendMessage(ChatColor.RED + "You are already op");
+                }
+ 
+            } else {
+                player.sendMessage(ChatColor.RED + "Application denied for user: " + player.getName() + "!");
+            }
+            return true;
+        }
+        return false;
+    }
 }
-
-	
